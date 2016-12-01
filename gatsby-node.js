2321fc22a7229865315a6bc6fdee7475b655a43c
Nic Gordon <gordonna@gmail.com>
2016-12-01 14:40:48 +1100

    Update sass resolver path

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require('path');
 exports.modifyWebpackConfig = function modifyWebpackConfig(config) {
   config.merge({
     sassLoader: {
-      includePaths: [path.resolve(__dirname, './styles')],
+      includePaths: [path.resolve(__dirname, 'styles')],
     },
   });
 
